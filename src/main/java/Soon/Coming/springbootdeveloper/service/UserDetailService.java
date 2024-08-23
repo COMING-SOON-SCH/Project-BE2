c9c1e9ae34a8fed45a27e2a906f3c69fad38fb13
CAFFEINETIME <lightme45@naver.com>
2024-08-24 03:19:08 +0900

    서비스 메서드 코드 작성

--- src/main/java/Soon/Coming/springbootdeveloper/service/UserDetailService.java
+++ src/main/java/Soon/Coming/springbootdeveloper/service/UserDetailService.java
@@ -4,14 +4,13 @@ import Soon.Coming.springbootdeveloper.domain.User;
 import Soon.Coming.springbootdeveloper.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-//JSDoc 처음 써보는데 이거이거 아주 깔@롱한데?
+//JSDoc 처음 써보는데 이거이거 아주 깔@롱한데? 멋져
 
-/**스프링 시큐리티에서 사용자 정보를 가져오는 인터페이스*/
+//스프링 시큐리티에서 사용자 정보를 가져오는 인터페이스
 public class UserDetailService implements UserDetailsService {
     private final UserRepository userRepository;
 
