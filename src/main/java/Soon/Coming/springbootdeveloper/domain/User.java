ce95d5fa27d2331d7ee5168af78e8aa4d056cb8e
CAFFEINETIME <lightme45@naver.com>
2024-08-23 19:05:16 +0900

    회원가입을 위하여 엔티티 구성중

--- src/main/java/Soon/Coming/springbootdeveloper/domain/User.java
+++ src/main/java/Soon/Coming/springbootdeveloper/domain/User.java
@@ -1,4 +1,73 @@
 package Soon.Coming.springbootdeveloper.domain;
 
-public class User {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.*; // JPA 관련 어노테이션을 임포트
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity // JPA 엔티티로 지정
+public class Member implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false) // 'updatable' 속성의 오타 수정
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    // 권한 반환
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    // 사용자의 username 반환 (고유한 값)
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    // 사용자의 비밀번호 반환
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    // 계정 만료 여부 반환
+    @Override
+    public boolean isAccountNonExpired() {
+        return true; // true -> 만료되지 않음
+    }
+
+    // 계정 잠금 여부 반환
+    @Override
+    public boolean isAccountNonLocked() {
+        return true; // true -> 잠금되지 않음
+    }
+
+    // 패스워드 만료 여부 반환
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true; // true -> 만료되지 않음
+    }
+
+    // 계정 사용 가능 여부 반환
+    @Override
+    public boolean isEnabled() {
+        return true; // true -> 사용 가능
+    }
 }
