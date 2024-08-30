fe587aacd613eae80ee054ac4b41c95a4d48ccb6
JooN104 <shjoonmd9@naver.com>
2024-08-30 21:37:50 +0900

    feat : post crud 구현 (신혜준)

---
+++ src/main/java/Soon/Coming/springbootdeveloper/post_crud/dto/UpdateArticleRequest.java
@@ -0,0 +1,13 @@
+package Soon.Coming.springbootdeveloper.post_crud.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateArticleRequest {
+    private String title;
+    private String content;
+}
