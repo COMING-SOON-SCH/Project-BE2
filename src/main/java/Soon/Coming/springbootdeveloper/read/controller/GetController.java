fde5dc012b1074c8559d523d0e70076d650c0e57
CAFFEINETIME <lightme45@naver.com>
2024-08-31 18:55:18 +0900

    feat: 빌드 설정 만지는중

--- src/main/java/Soon/Coming/springbootdeveloper/read/controller/GetController.java
+++ src/main/java/Soon/Coming/springbootdeveloper/read/controller/GetController.java
@@ -1,10 +1,13 @@
 package Soon.Coming.springbootdeveloper.read.controller;
 
+import Soon.Coming.springbootdeveloper.read.service.GetService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class GetController {
-    private final GetController getController;
+    private final GetService getService;
+
 }
