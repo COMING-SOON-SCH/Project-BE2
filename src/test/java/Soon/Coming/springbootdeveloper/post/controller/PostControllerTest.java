92c33f6df65d7409b7c06599e919aecfc4c655c2
CAFFEINETIME <lightme45@naver.com>
2024-08-31 18:12:14 +0900

    feat : 포스트 테스트 코드 제작완료, 오류 수정중

--- src/test/java/Soon/Coming/springbootdeveloper/post/controller/PostControllerTest.java
+++ src/test/java/Soon/Coming/springbootdeveloper/post/controller/PostControllerTest.java
@@ -1,6 +1,9 @@
 package Soon.Coming.springbootdeveloper.post.controller;
 
+import Soon.Coming.springbootdeveloper.post.domain.Post;
+import Soon.Coming.springbootdeveloper.post.dto.CreatePostRequest;
 import Soon.Coming.springbootdeveloper.post.repository.PostRepository;
+import Soon.Coming.springbootdeveloper.userSignup.domain.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -8,11 +11,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest //테스트용 애플리케이션 컨텍스트
 @AutoConfigureMockMvc
@@ -44,5 +54,26 @@ class PostControllerTest {
         final String url = "/posts";
         final String title = "title";
         final String content = "content";
+        final Long categoryId = 1L;
+        final User user = new User();
+        final CreatePostRequest createPostRequest = new CreatePostRequest(title, content, categoryId);
+
+        //객체 JSON으로 직렬화
+        final String requestBody = objectMapper.writeValueAsString(createPostRequest);
+
+        //when
+        ResultActions result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(requestBody)
+        );
+
+        //then
+        result.andExpect(status().isCreated());
+
+        List<Post> posts = postRepository.findAll();
+
+        assertThat(posts.size()).isEqualTo(1);
+        assertThat(posts.get(0).getTitle()).isEqualTo(title);
+        assertThat(posts.get(0).getContent()).isEqualTo(content);
     }
 }
