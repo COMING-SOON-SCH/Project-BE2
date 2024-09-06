856d59167edc4c996a16bde4fb2056902e5f5449
JooN104 <shjoonmd9@naver.com>
2024-09-06 17:06:46 +0900

    feat : 서버 구동 완료

--- src/main/java/Soon/Coming/springbootdeveloper/post/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package Soon.Coming.springbootdeveloper.post.repository;
-
-import Soon.Coming.springbootdeveloper.post.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Article, Long> {
-}
