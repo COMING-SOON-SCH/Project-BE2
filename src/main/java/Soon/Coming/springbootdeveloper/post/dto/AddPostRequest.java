6340db56f07cf12fddde43194c9a39b6d68a0b34
JooN104 <shjoonmd9@naver.com>
2024-09-15 16:39:41 +0900

    feat : post 기본 crud api 구현

--- src/main/java/Soon/Coming/springbootdeveloper/post/dto/AddPostRequest.java
+++ src/main/java/Soon/Coming/springbootdeveloper/post/dto/AddPostRequest.java
@@ -1,6 +1,6 @@
 package Soon.Coming.springbootdeveloper.post.dto;
 
-import user.domain.User;
+import Soon.Coming.springbootdeveloper.user.domain.User;
 import Soon.Coming.springbootdeveloper.post.domain.Post;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -8,8 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
-@AllArgsConstructor
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -19,26 +18,10 @@ public class AddPostRequest {
     @Size(min=1, max=5)
     private String title;
     private String content;
-
-    @NotBlank
-    private Long user_id;
-
-    @NotBlank
-    private String email;
-
-    @Positive
-    private Long categoryId;
-
-
-
     public Post toEntity() {
-        //???ì¤???? ??????
-        User userExample = new User(1L, "Ogu", "password");
         return Post.builder()
                 .title(title)
                 .content(content)
-                .user_id(user_id)
-
                 .build();
     }
 }
